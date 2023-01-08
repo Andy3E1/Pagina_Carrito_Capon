a524eb861cc7271bdc7159b4d5965ef6ac6612ca
unknown <Andy3E1>
2023-01-08 13:39:54 -0500

    Funcionamiento_Carro

--- src/app/Componentes/carrito/carrito.module.ts
+++ src/app/Componentes/carrito/carrito.module.ts
@@ -1,7 +1,7 @@
-import { NgModule} from '@angular/core';
-import { CommonModule} from '@angular/common';
-import { Router } from '@angular/router';
-declare function funcion():void; 
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @NgModule({
   declarations: [],
@@ -10,29 +10,5 @@ declare function funcion():void;
   ]
 })
 export class CarritoModule {
-  constructor(private router:Router){      
-    
-  }
-  Nombre:any
-  contraseña:any
-  si_comprar='Comprado';
-  select_comprar='Comprar';
-  comprar=0;
-ngOnInit():void{
-  console.log("Ejecuta");
-    this.recuperar_Formulario();
-  funcion();
-}
-recuperar_Formulario(){
-  this.Nombre= localStorage.getItem('Nombre')
-  this.contraseña= localStorage.getItem('contraseña')}
-nav(){
-  this.router.navigate([''])
-}
-carro(){
-  this.router.navigate(['web2'])
-}
-limpia(){
-  this.router.navigate([''])
-}
-}
+  
+ }
